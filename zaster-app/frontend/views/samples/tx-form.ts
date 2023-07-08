b17b4789bc493d458d52a6a3e4ab550b3730aaa1
hajo70 <hessmann@spricom.de>
2023-07-08 09:25:56 +0200

    Render bookings

--- zaster-app/frontend/views/samples/tx-form.ts
+++ zaster-app/frontend/views/samples/tx-form.ts
@@ -2,12 +2,16 @@ import {html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import {View} from 'Frontend/views/view';
 
+import '@vaadin/button';
 import '@vaadin/text-field';
 import '@vaadin/date-time-picker';
 
-import {Binder, field} from '@hilla/form';
+import {Binder, BinderNode, field} from '@hilla/form';
 import TransactionDtoModel from "Frontend/generated/de/spricom/zaster/dtos/tracking/TransactionDtoModel.ts";
 import TransactionDto from "Frontend/generated/de/spricom/zaster/dtos/tracking/TransactionDto.ts";
+import {repeat} from "lit/directives/repeat.js";
+import BookingDto from "Frontend/generated/de/spricom/zaster/dtos/tracking/BookingDto.ts";
+import BookingDtoModel from "Frontend/generated/de/spricom/zaster/dtos/tracking/BookingDtoModel.ts";
 
 @customElement('tx-form')
 export class TxForm extends View {
@@ -33,6 +37,22 @@ export class TxForm extends View {
                     style="--vaadin-field-default-width: 24em"
                     ${field(model.description)}
             ></vaadin-text-field>
+            ${repeat(this.binder.model.bookings, this.renderBooking)}
+            <vaadin-button
+                    @click=${() => this.binder.for(this.binder.model.bookings).appendItem()}
+            >
+                <vaadin-icon icon="lumo:plus"></vaadin-icon>
+            </vaadin-button>
+        `;
+    }
+
+    private renderBooking(bookingBinder: BinderNode<BookingDto, BookingDtoModel>) {
+        return html`
+            <span>Booking</span>
+            <vaadin-date-time-picker
+                    label="Buchungszeitpunkt"
+                    ${field(bookingBinder.model.bookedAt)}
+            ></vaadin-date-time-picker>
         `;
     }
 
