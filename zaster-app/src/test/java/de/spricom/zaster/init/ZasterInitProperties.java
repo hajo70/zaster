8aa591ed37b82b6cc136168163fe396181f38bd6
hajo70 <hessmann@spricom.de>
2023-07-15 22:05:06 +0200

    Zaster init sample

---
+++ zaster-app/src/test/java/de/spricom/zaster/init/ZasterInitProperties.java
@@ -0,0 +1,60 @@
+package de.spricom.zaster.init;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(prefix = "zaster.init")
+@Data
+public class ZasterInitProperties {
+
+    private Map<String, Tenant> tenants;
+
+    @Data
+    static class Tenant {
+        private String name;
+        private Locale locale;
+        private ZoneId timezone;
+        private Map<String, User> users;
+        private List<String> isoCurrencies;
+        private Map<String, Currency> currencies;
+        private Map<String, Account> accounts;
+        private Map<String, Import> imports;
+    }
+
+
+    @Data
+    static class User {
+        enum Role {USER, ADMIN}
+
+        private String name;
+        private Role role;
+    }
+
+    @Data
+    static class Currency {
+        enum Type {CRYPTO, FIAT, METAL}
+
+        private String name;
+        private Type type;
+    }
+
+    @Data
+    static class Account {
+        private String name;
+        private Map<String, Account> accounts;
+    }
+
+    @Data
+    static class Import {
+        private String importer;
+        private List<File> files;
+    }
+}
---
+++ zaster-app/src/test/java/de/spricom/zaster/init/ZasterInitTest.java
@@ -0,0 +1,51 @@
+package de.spricom.zaster.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.ZoneId;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("init")
+public class ZasterInitTest {
+
+    @Autowired
+    private ZasterInitProperties props;
+
+    @Test
+    void checkProperties() {
+        assertThat(props).isNotNull();
+        assertThat(props.getTenants()).hasSize(2);
+        var me = props.getTenants().get("me");
+        assertThat(me.getName()).isEqualTo("My personal Zaster");
+        assertThat(me.getLocale()).isEqualTo(Locale.US);
+        assertThat(me.getTimezone()).isEqualTo(ZoneId.of("GMT"));
+        assertThat(me.getUsers()).hasSize(2)
+                .containsKey("admin");
+        assertThat(me.getUsers().get("admin").getName()).isEqualTo("Emma Powerful");
+        assertThat(me.getUsers().get("admin").getRole()).isEqualTo(ZasterInitProperties.User.Role.ADMIN);
+
+        assertThat(me.getIsoCurrencies()).contains("EUR", "USD");
+        assertThat(me.getCurrencies()).isNotEmpty();
+        var btc = me.getCurrencies().get("BTC");
+        assertThat(btc.getName()).isEqualTo("Bitcoin");
+        assertThat(btc.getType()).isEqualTo(ZasterInitProperties.Currency.Type.CRYPTO);
+
+        assertThat(me.getAccounts()).isNotEmpty();
+        var accounts = me.getAccounts().get("accounts");
+        assertThat(accounts.getName()).isEqualTo("My bank accounts");
+        assertThat(accounts.getAccounts()).isNotEmpty();
+        assertThat(accounts.getAccounts().get("bank").getName()).isEqualTo("My bank");
+
+        assertThat(me.getImports()).isNotEmpty();
+        var imp = me.getImports().get("bank");
+        assertThat(imp.getImporter()).isEqualTo("My bank CSV importer");
+        assertThat(imp.getFiles()).hasSize(2);
+        assertThat(imp.getFiles().get(0)).hasFileName("bank1.csv");
+    }
+}
