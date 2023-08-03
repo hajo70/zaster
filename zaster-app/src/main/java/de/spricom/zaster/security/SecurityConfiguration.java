3a8994b56eae1ff5c53e67b527e33a7361513d06
hajo70 <hessmann@spricom.de>
2023-08-03 21:14:54 +0200

    H2 console integrated

--- zaster-app/src/main/java/de/spricom/zaster/security/SecurityConfiguration.java
+++ zaster-app/src/main/java/de/spricom/zaster/security/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -36,11 +37,16 @@ public class SecurityConfiguration extends VaadinWebSecurity {
         http.authorizeHttpRequests(reg ->
                 reg.requestMatchers(
                         new AntPathRequestMatcher("/images/*.png"),
-                        new AntPathRequestMatcher("/line-awesome/**.svg")
+                        new AntPathRequestMatcher("/line-awesome/**.svg"),
+                        new AntPathRequestMatcher("/h2-console/**")
                 ).permitAll())
                 .authorizeHttpRequests(reg ->
                         reg.requestMatchers(new AntPathRequestMatcher("/api/**"))
                                 .hasRole("ADMIN"));
+
+        http.csrf(reg ->
+                reg.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")));
+        http.headers(reg -> reg.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
 
         super.configure(http);
 
