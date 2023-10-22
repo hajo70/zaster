d7b574f635338fcf8c6c564f5aa5857dce4d5a01
hajo70 <hessmann@spricom.de>
2023-10-22 20:07:39 +0200

    Attach account to handler

--- zaster-app/frontend/views/bookings/bookings-view.ts
+++ zaster-app/frontend/views/bookings/bookings-view.ts
@@ -1,4 +1,4 @@
-import {customElement, query, state} from "lit/decorators.js";
+import {customElement, query} from "lit/decorators.js";
 import {View} from "Frontend/views/view.ts";
 import {html} from "lit";
 import {bookingsViewStore} from "Frontend/views/bookings/bookings-view-store.ts";
@@ -10,14 +10,14 @@ import '@vaadin/grid/vaadin-grid-sort-column';
 import '@vaadin/split-layout';
 import '@vaadin/icon'
 import '@vaadin/menu-bar'
-import '@vaadin/context-menu'
 import {Notification} from "@vaadin/notification";
 
 import {TabsSelectedChangedEvent} from "@vaadin/tabs";
 import {Grid} from "@vaadin/grid";
 import './account-form';
 import CurrencyDto from "Frontend/generated/de/spricom/zaster/dtos/settings/CurrencyDto.ts";
-import {columnBodyRenderer} from "@vaadin/grid/lit";
+import {columnBodyRenderer, GridColumnBodyLitRenderer} from "@vaadin/grid/lit";
+import {Account} from "Frontend/model/tracking/Account.ts";
 
 interface Item {
     text: string,
@@ -29,12 +29,6 @@ export class BookingsView extends View {
 
     @query("vaadin-grid.accounts-grid")
     accountsGrid!: Grid;
-
-    @state()
-    private items : Item[] = [
-        {text: 'View', handler: this.handleView},
-        {text: 'Edit', handler: this.handleEdit},
-        {text: 'Delete', handler: this.handleDelete}];
 
     protected override render() {
         return html`
@@ -51,34 +45,20 @@ export class BookingsView extends View {
                             <vaadin-icon icon="lumo:plus"></vaadin-icon>
                         </vaadin-button>
                     </div>
-                    <vaadin-context-menu .items=${this.items} 
-                                         class="h-full"
-                                         @item-selected=${this.handleItemSelected}
+                    <vaadin-grid theme="compact"
+                                 class="accounts-grid h-full"
+                                 .itemHasChildrenPath="${'hasChildren'}"
+                                 .dataProvider="${bookingsViewStore.dataProvider}"
+                                 .selectedItems=${[bookingsViewStore.selectedAccount]}
+                                 @active-item-changed=${this.handleGridSelection}
                     >
-                        <vaadin-grid theme="compact"
-                                     class="accounts-grid h-full"
-                                     .itemHasChildrenPath="${'hasChildren'}"
-                                     .dataProvider="${bookingsViewStore.dataProvider}"
-                                     .selectedItems=${[bookingsViewStore.selectedAccount]}
-                                     @active-item-changed=${this.handleGridSelection}
-                        >
-                            <vaadin-grid-tree-column path="accountName"></vaadin-grid-tree-column>
-                            <vaadin-grid-column
-                                    width="50px"
-                                    flex-grow="0"
-                                    ${columnBodyRenderer(
-                                            () => html`
-                                                <vaadin-menu-bar .items=${this.items}
-                                                                 theme="tertiary"
-                                                                 @item-selected=${this.handleItemSelected}
-                                                >
-                                                </vaadin-menu-bar>
-                                            `,
-                                            []
-                                    )}
-                            ></vaadin-grid-column>
-                        </vaadin-grid>
-                    </vaadin-context-menu>
+                        <vaadin-grid-tree-column path="accountName"></vaadin-grid-tree-column>
+                        <vaadin-grid-column
+                                width="50px"
+                                flex-grow="0"
+                                ${columnBodyRenderer(this.menuRenderer, [])}
+                        ></vaadin-grid-column>
+                    </vaadin-grid>
                 </div>
                 <div class="flex flex-col h-full flex-grow">
                     <vaadin-grid .items="${bookingsViewStore.transfers}" theme="compact" column-reordering-allowed>
@@ -116,22 +96,39 @@ export class BookingsView extends View {
         bookingsViewStore.setSelectedAccount(ev.detail.value);
     }
 
+    private menuRenderer: GridColumnBodyLitRenderer<Account> = (account) => {
+        let menuItems : Item[] = [
+            {text: 'Add', handler: () => this.onAdd(account)},
+            {text: 'Edit', handler: () => this.onEdit(account)}
+        ];
+        if (!account.hasChildren) {
+            menuItems.push(
+                {text: 'Delete', handler: () => this.onDelete(account)}
+            )
+        }
+        return html`
+        <vaadin-menu-bar .items=${menuItems}
+                         theme="tertiary"
+                         @item-selected=${this.handleItemSelected}
+        >
+        </vaadin-menu-bar>
+    `;}
+
     handleItemSelected(ev: CustomEvent) {
-        this.items
-            .filter(item => item.text === ev.detail.value.text)
-            .forEach(item => item.handler())
+        const item = ev.detail.value as Item;
+        item.handler();
     }
 
-    handleView() {
-        Notification.show("View!");
+    onAdd(account: Account) {
+        Notification.show("Add: " + account.accountName);
     }
 
-    handleEdit() {
-        Notification.show("Edit!");
+    onEdit(account: Account) {
+        Notification.show("Edit: " + account.accountName);
     }
 
-    handleDelete() {
-        Notification.show("Delete!");
+    onDelete(account: Account) {
+        Notification.show("Delete: " + account.accountName);
     }
 
     selectedChanged(e: TabsSelectedChangedEvent) {
